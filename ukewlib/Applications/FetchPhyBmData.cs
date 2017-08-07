5c07c9024fb2ddd53484cae646c9b5650ff1f1fb
Chris Bacon <chrisbacon@google.com>
2017-08-07 11:07:54 +0100

    Physical data fetcher, with test

---
+++ ukewlib/Applications/FetchPhyBmData.cs
@@ -0,0 +1,45 @@
+using System.Threading;
+using System.Threading.Tasks;
+using NodaTime;
+using Ukew.Elexon;
+using Ukew.Utils;
+using Ukew.Utils.Tasks;
+
+namespace Ukew.Applications
+{
+    public class FetchPhyBmData
+    {
+        public FetchPhyBmData(ITaskHelper taskHelper, IElexonDownloader downloader, IDirectory dir, ITime time)
+        {
+            _taskHelper = taskHelper;
+            _time = time;
+            _scheduler = new Scheduler(time, taskHelper);
+            _phyBmData = new PhyBmData(taskHelper, downloader);
+            _writer = new PhyBmData.FpnWriter(taskHelper, dir);
+
+        }
+
+        private readonly ITaskHelper _taskHelper;
+        private readonly ITime _time;
+        private readonly Scheduler _scheduler;
+        private readonly PhyBmData _phyBmData;
+        private readonly PhyBmData.FpnWriter _writer;
+
+        public async Task Start(bool startImmediately, CancellationToken ct = default(CancellationToken))
+        {
+            bool wait = !startImmediately;
+            while (true)
+            {
+                if (wait)
+                {
+                    await _scheduler.ScheduleOne(Duration.FromMinutes(30), Duration.FromMinutes(20), ct);
+                }
+                wait = true;
+                // Fetch for the next settlement period. This should be ready 10 minutes beforehand.
+                Instant fetchTime = _time.GetCurrentInstant() + Duration.FromMinutes(30);
+                var data = await _phyBmData.GetAsync(fetchTime.SettlementDate(), fetchTime.SettlementPeriod(), ct);
+                await _writer.AppendAsync(data, ct);
+            }
+        }
+    }
+}
---
+++ ukewlib_test/Applications/FetchPhyBmDataTest.cs
@@ -0,0 +1,42 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Ukew.Elexon;
+using Ukew.Storage;
+using Ukew.Testing;
+using Ukew.Utils.Tasks;
+using Xunit;
+
+namespace Ukew.Applications
+{
+    public class FetchPhyBmDataTest
+    {
+        [Fact]
+        public void Fetch()
+        {
+            TimeRunner.Run(async (time, th) =>
+            {
+                var downloader = new FakeElexonDownloader();
+                var dir = new FakeDirectory();
+                var fetcher = new FetchPhyBmData(th, downloader, dir, time);
+
+                var cts = new CancellationTokenSource();
+                Task unused = th.Run(async () =>
+                {
+                    await th.Delay(NodaTime.Duration.FromMinutes(60)).ConfigureAwait(th);
+                    cts.Cancel();
+                });
+
+                await fetcher.Start(false, cts.Token).ConfigureAwaitHideCancel(th);
+
+                var reader = new PhyBmData.FpnReader(th, dir);
+                Assert.Equal(1031 + 1023, await reader.CountAsync().ConfigureAwait(th));
+                var cldcwIdHash = BmUnitIds.Hash("T_CLDCW-1");
+                var cldcwTask = (await reader.ReadAsync().ConfigureAwait(th)).Where(x => x.BmUnitIdHash == cldcwIdHash);
+                var cldcw = await cldcwTask.ToList().ConfigureAwait(th);
+                Assert.Equal(4, cldcw.Count);
+                Assert.Equal(new [] { 13.0, 11.0, 11.0, 9.0 }, cldcw.Select(x => x.LevelTo.Megawatts));
+            }, startInstant: NodaTime.Instant.FromUtc(2017, 8, 5, 22, 29));
+        }
+    }
+}
