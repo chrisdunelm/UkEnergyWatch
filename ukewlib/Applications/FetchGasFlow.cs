904de425d141f61205b6e428da84cede0ed9c94d
Chris Bacon <chrisbacon@google.com>
2017-09-21 22:39:13 +0100

    FetchGasFlow lib application class + test

---
+++ ukewlib/Applications/FetchGasFlow.cs
@@ -0,0 +1,56 @@
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NodaTime;
+using Ukew.NationalGrid;
+using Ukew.Storage;
+using Ukew.Utils;
+using Ukew.Utils.Tasks;
+
+namespace Ukew.Applications
+{
+    public class FetchGasFlow
+    {
+        public FetchGasFlow(ITaskHelper taskHelper, ISoapDownloader downloader, IDirectory dir, ITime time)
+        {
+            _taskHelper = taskHelper;
+            _time = time;
+            _scheduler = new Scheduler(time, taskHelper);
+            _flow = new InstantaneousFlow(taskHelper, downloader);
+            _reader = new InstantaneousFlow.Reader(taskHelper, dir);
+            _writer = new InstantaneousFlow.Writer(taskHelper, dir);
+            _strings = new Strings(taskHelper, dir);
+        }
+
+        private readonly ITaskHelper _taskHelper;
+        private readonly ITime _time;
+        private readonly Scheduler _scheduler;
+        private readonly InstantaneousFlow _flow;
+        private readonly InstantaneousFlow.Reader _reader;
+        private readonly InstantaneousFlow.Writer _writer;
+        private readonly Strings _strings;
+
+        public async Task Start(bool startImmediately, CancellationToken ct = default(CancellationToken))
+        {
+            bool wait = !startImmediately;
+            while (true)
+            {
+                if (wait)
+                {
+                    await _scheduler.ScheduleOne(Duration.FromMinutes(12), Duration.FromMinutes(2.2), ct).ConfigureAwait(_taskHelper);
+                }
+                wait = true;
+                var lastData = await (await _reader.ReadAsync((int)await _reader.CountAsync(ct).ConfigureAwait(_taskHelper) - 1, ct: ct)
+                    .ConfigureAwait(_taskHelper)).FirstOrDefault(ct).ConfigureAwait(_taskHelper);
+                var tries = 50; // 10 minutes
+                while (--tries >= 0 && await _flow.GetLatestPublicationTimeAsync(ct).ConfigureAwait(_taskHelper) < lastData.Update)
+                {
+                    await _taskHelper.Delay(Duration.FromMinutes(0.2)).ConfigureAwait(_taskHelper);
+                }
+                var datas0 = await _flow.GetInstantaneousFlowDataAsync(ct).ConfigureAwait(_taskHelper);
+                var datas1 = await datas0.WriteStringsAsync(_taskHelper, _strings).ConfigureAwait(_taskHelper);
+                await _writer.AppendAsync(datas1, ct).ConfigureAwait(_taskHelper);
+            }
+        }
+    }
+}
--- ukewlib_test/Applications/FetchGasFlowTest.cs
+++ ukewlib_test/Applications/FetchGasFlowTest.cs
@@ -1,21 +1,22 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using NodaTime;
+using Ukew.NationalGrid;
 using Ukew.Storage;
 using Ukew.Testing;
 using Ukew.Utils.Tasks;
 using UnitsNet;
 using Xunit;
 
-namespace Ukew.NationalGrid
+namespace Ukew.Applications
 {
-    public class InstantaneousFlowTest
+    public class FetchGasFlowTest
     {
         [Fact]
-        public void GetLatestPublicationTime()
+        public void Fetch()
         {
             TimeRunner.Run(async (time, th) =>
             {
@@ -24,32 +25,28 @@ namespace Ukew.NationalGrid
                     { ("http://energywatch.natgrid.co.uk/EDP-PublicUI/PublicPI/InstantaneousFlowWebService.asmx",
                         "http://www.NationalGrid.com/EDP/UI/GetLatestPublicationTime",
                         "<GetLatestPublicationTime xmlns=\"http://www.NationalGrid.com/EDP/UI/\"/>"),
-                        Soap.LoadBody("GetLatestPublicationTime1") }
-                };
-                var fakeSoapDownloader = new FakeSoapDownloader(entries);
-                var flow = new InstantaneousFlow(th, fakeSoapDownloader);
-                var instant = await flow.GetLatestPublicationTimeAsync().ConfigureAwait(th);
-                Assert.Equal(Instant.FromUtc(2017, 9, 7, 17, 23, 0), instant);
-            });
-        }
-
-        [Fact]
-        public void GetInstantaneousFlowData()
-        {
-            TimeRunner.Run(async (time, th) =>
-            {
-                var entries = new Dictionary<(string uri, string soapAction, string soapBody), XElement>
-                {
+                        Soap.LoadBody("GetLatestPublicationTime1") },
                     { ("http://energywatch.natgrid.co.uk/EDP-PublicUI/PublicPI/InstantaneousFlowWebService.asmx",
                         "http://www.NationalGrid.com/EDP/UI/GetInstantaneousFlowData",
                         "<GetInstantaneousFlowData xmlns=\"http://www.NationalGrid.com/EDP/UI/\" />"),
                         Soap.LoadBody("GetInstantaneousFlowData1") }
                 };
-                var fakeSoapDownloader = new FakeSoapDownloader(entries);
-                var flow = new InstantaneousFlow(th, fakeSoapDownloader);
-                var stringsDir = new FakeDirectory();
-                var strings = new Strings(th, stringsDir);
-                var data = await (await flow.GetInstantaneousFlowDataAsync().ConfigureAwait(th)).WriteStringsAsync(th, strings).ConfigureAwait(th);
+                var downloader = new FakeSoapDownloader(entries);
+                var dir = new FakeDirectory();
+                var fetcher = new FetchGasFlow(th, downloader, dir, time);
+
+                var cts = new CancellationTokenSource();
+                Task unused = th.Run(async () =>
+                {
+                    await th.Delay(NodaTime.Duration.FromMinutes(15)).ConfigureAwait(th);
+                    cts.Cancel();
+                });
+                await fetcher.Start(false, cts.Token).ConfigureAwaitHideCancel(th);
+
+                var reader = new InstantaneousFlow.Reader(th, dir);
+                var strings = new Strings(th, dir);
+                Assert.Equal(258, await reader.CountAsync().ConfigureAwait(th));
+                var data = await (await reader.ReadAsync().ConfigureAwait(th)).ToList().ConfigureAwait(th);
                 Assert.Equal(258, data.Count);
                 var nameIndex0 = await strings.AddOrGetIndexAsync("ALDBROUGH").ConfigureAwait(th);
                 Assert.Equal(new InstantaneousFlow.Data(Instant.FromUtc(2017, 9, 15, 7, 24),
@@ -57,7 +54,7 @@ namespace Ukew.NationalGrid
                 var nameIndex1 = await strings.AddOrGetIndexAsync("TOTAL SUPPLY").ConfigureAwait(th);
                 Assert.Equal(new InstantaneousFlow.Data(Instant.FromUtc(2017, 9, 15, 7, 34),
                     InstantaneousFlow.SupplyType.TotalSupply, (ushort)nameIndex1, Flow.FromCubicMetersPerHour(190.03944 * 1e6 / 24)), data.Last());
-            });
+            }, startInstant: NodaTime.Instant.FromUtc(2017, 9, 7, 17, 23));
         }
     }
 }
