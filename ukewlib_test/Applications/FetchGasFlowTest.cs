b20ef3a50187c96357c8dbe793ba8be7495b7765
Chris Bacon <chrisbacon@google.com>
2017-09-28 17:17:27 +0100

    Fix gas tests, after change to UTC

--- ukewlib_test/Applications/FetchGasFlowTest.cs
+++ ukewlib_test/Applications/FetchGasFlowTest.cs
@@ -49,12 +49,12 @@ namespace Ukew.Applications
                 var data = await (await reader.ReadAsync().ConfigureAwait(th)).ToList().ConfigureAwait(th);
                 Assert.Equal(258, data.Count);
                 var nameIndex0 = await strings.AddOrGetIndexAsync("ALDBROUGH").ConfigureAwait(th);
-                Assert.Equal(new InstantaneousFlow.Data(Instant.FromUtc(2017, 9, 15, 7, 24),
+                Assert.Equal(new InstantaneousFlow.Data(Instant.FromUtc(2017, 9, 15, 6, 24),
                     InstantaneousFlow.SupplyType.ZoneSupply, (ushort)nameIndex0, Flow.Zero), data.First());
                 var nameIndex1 = await strings.AddOrGetIndexAsync("TOTAL SUPPLY").ConfigureAwait(th);
-                Assert.Equal(new InstantaneousFlow.Data(Instant.FromUtc(2017, 9, 15, 7, 34),
+                Assert.Equal(new InstantaneousFlow.Data(Instant.FromUtc(2017, 9, 15, 6, 34),
                     InstantaneousFlow.SupplyType.TotalSupply, (ushort)nameIndex1, Flow.FromCubicMetersPerHour(190.03944 * 1e6 / 24)), data.Last());
-            }, startInstant: NodaTime.Instant.FromUtc(2017, 9, 7, 17, 23));
+            }, startInstant: NodaTime.Instant.FromUtc(2017, 9, 7, 16, 23));
         }
     }
 }
--- ukewlib_test/NationalGrid/InstantaneousFlowTest.cs
+++ ukewlib_test/NationalGrid/InstantaneousFlowTest.cs
@@ -29,7 +29,7 @@ namespace Ukew.NationalGrid
                 var fakeSoapDownloader = new FakeSoapDownloader(entries);
                 var flow = new InstantaneousFlow(th, fakeSoapDownloader);
                 var instant = await flow.GetLatestPublicationTimeAsync().ConfigureAwait(th);
-                Assert.Equal(Instant.FromUtc(2017, 9, 7, 17, 23, 0), instant);
+                Assert.Equal(Instant.FromUtc(2017, 9, 7, 16, 23, 0), instant);
             });
         }
 
@@ -52,10 +52,10 @@ namespace Ukew.NationalGrid
                 var data = await (await flow.GetInstantaneousFlowDataAsync().ConfigureAwait(th)).WriteStringsAsync(th, strings).ConfigureAwait(th);
                 Assert.Equal(258, data.Count);
                 var nameIndex0 = await strings.AddOrGetIndexAsync("ALDBROUGH").ConfigureAwait(th);
-                Assert.Equal(new InstantaneousFlow.Data(Instant.FromUtc(2017, 9, 15, 7, 24),
+                Assert.Equal(new InstantaneousFlow.Data(Instant.FromUtc(2017, 9, 15, 6, 24),
                     InstantaneousFlow.SupplyType.ZoneSupply, (ushort)nameIndex0, Flow.Zero), data.First());
                 var nameIndex1 = await strings.AddOrGetIndexAsync("TOTAL SUPPLY").ConfigureAwait(th);
-                Assert.Equal(new InstantaneousFlow.Data(Instant.FromUtc(2017, 9, 15, 7, 34),
+                Assert.Equal(new InstantaneousFlow.Data(Instant.FromUtc(2017, 9, 15, 6, 34),
                     InstantaneousFlow.SupplyType.TotalSupply, (ushort)nameIndex1, Flow.FromCubicMetersPerHour(190.03944 * 1e6 / 24)), data.Last());
             });
         }
