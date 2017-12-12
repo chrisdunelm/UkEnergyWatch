2ab9d26a6bdf539c5f328be657eb57c7d11d4855
Chris Bacon <chrisbacon@google.com>
2017-12-12 21:21:37 +0000

    Allow freq collection to skip missing data

--- ukewlib/Applications/FetchFreq.cs
+++ ukewlib/Applications/FetchFreq.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -31,11 +32,15 @@ namespace Ukew.Applications
         public async Task Start(bool startImmediately, CancellationToken ct = default(CancellationToken))
         {
             bool wait = !startImmediately;
+            Duration getOfs = Duration.Zero;
+            Duration? waitOverride = null;
             while (true)
             {
                 if (wait)
                 {
-                    await _scheduler.ScheduleOne(Duration.FromMinutes(2), Duration.FromSeconds(4), ct).ConfigureAwait(_taskHelper);
+                    Console.WriteLine("Waiting...");
+                    await _scheduler.ScheduleOne(waitOverride ?? Duration.FromMinutes(2), Duration.FromSeconds(4), ct).ConfigureAwait(_taskHelper);
+                    Console.WriteLine("Wait complete");
                 }
                 wait = true;
                 var count = (int)await _reader.CountAsync(ct).ConfigureAwait(_taskHelper);
@@ -44,7 +49,7 @@ namespace Ukew.Applications
                 if (count > 0)
                 {
                     var last = await (await _reader.ReadAsync(count - 1).ConfigureAwait(_taskHelper)).Last().ConfigureAwait(_taskHelper);
-                    from = last.Update;
+                    from = last.Update + getOfs;
                 }
                 else
                 {
@@ -55,8 +60,24 @@ namespace Ukew.Applications
                 {
                     to = now;
                 }
+                Console.WriteLine($"About to Get data. from:{from}; to:{to}");
                 var data = await _freq.GetAsync(from + Duration.FromSeconds(1), to, ct).ConfigureAwait(_taskHelper);
+                Console.WriteLine($"Got data. Count:{data.Count}");
+                if (data.Count == 0 && to < now - Duration.FromDays(1))
+                {
+                    // If no data, and we're at least a day in the past, then skip forward an hour.
+                    // This is probably because data is missing.
+                    getOfs += Duration.FromHours(1);
+                    waitOverride = Duration.FromSeconds(20);
+                    Console.WriteLine($"Increasing getOfs to:{getOfs}");
+                }
+                else
+                {
+                    getOfs = Duration.Zero;
+                    waitOverride = null;
+                }
                 await _writer.AppendAsync(data, ct).ConfigureAwait(_taskHelper);
+                Console.WriteLine("Append complete");
             }
         }
     }
