2ee6dd4e524101f7938fb545db564eaddd24225e
Chris Bacon <chris@dunelm.org.uk>
2018-07-14 00:03:59 +0100

    Add Coal and CCGT CO2 calculations
    Although it needs improving, to show the method

--- ukewlib/Utils/UnitsExtensions.cs
+++ ukewlib/Utils/UnitsExtensions.cs
@@ -4,9 +4,12 @@ namespace Ukew.Utils
 {
     public static class UnitsExtensions
     {
-        public static MassFlow CcgtCo2(this Power power) => MassFlow.Zero;
+        // TODO: Calculate these properly!
+        private static MassFlow CalculateCo2(this Power power, double wToKgPerS) => MassFlow.FromKilogramsPerSecond(power.Watts * wToKgPerS);
+
+        public static MassFlow CcgtCo2(this Power power) => power.CalculateCo2(1.02006335797254e-7);
         public static MassFlow OcgtCo2(this Power power) => MassFlow.Zero;
         public static MassFlow OilCo2(this Power power) => MassFlow.Zero;
-        public static MassFlow CoalCo2(this Power power) => MassFlow.Zero;
+        public static MassFlow CoalCo2(this Power power) => power.CalculateCo2(2.71604938271605e-7);
     }
 }
