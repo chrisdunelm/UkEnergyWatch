1d3cac593bd68e3575d9e0d1f7d358fc6812563f
Chris Bacon <chris@dunelm.org.uk>
2018-11-30 22:42:45 +0000

    Delete old www project

--- ukew_www/Utils/Extensions.cs
+++
@@ -1,9 +0,0 @@
-using UnitsNet;
-
-namespace Ukew.Utils
-{
-    public static class Extensions
-    {
-        public static MassFlow CalculateCo2(this Power power, double wToKgPerS) => MassFlow.FromKilogramsPerSecond(power.Watts * wToKgPerS);
-    }
-}
