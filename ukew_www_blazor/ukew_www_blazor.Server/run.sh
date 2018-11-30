241b42ab618a0fae90e785d70f376ac392882af1
Chris Bacon <chris@dunelm.org.uk>
2018-11-30 21:50:23 +0000

    Add run.sh to pass test data

---
+++ ukew_www_blazor/ukew_www_blazor.Server/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+dotnet run -- --fuelinsthhcurdatadirectory ../../testdata/FuelInstHhCur --freqdatadirectory ../../testdata/Freq --gasflowdatadirectory ../../testdata/gasflow --b1610datadirectory ../../testdata/B1610 --fpndatadirectory ../../testdata/Fpn
